aafe4a049642a0b5ee2e755020d39a3f2e529b5c
Jeremey Hansen <jeremeyhansen@yahoo.com>
2023-04-10 16:06:18 -0700

    added pics and animations to home and services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,29 +1,55 @@
-import '../css/service.css'
+import "../css/service.css";
+import PressureWasher from "../pressure-washer.png";
+import PressureWasher2 from "../pressure-washer (3).png";
+import { Link } from "react-router-dom";
 
-export default function Services(){
-    return (
-        <div className="service-container1">
-        <h1 className="service-title">Services</h1> 
-        <div className="service-container">
-            <div>
-                <h3>Pressure Cleaning</h3>
-                <p>Residential</p>
-                <p>Commercial</p>
-                <p>Roof Cleaning</p>
-            </div>
-            <div>
-                <h3>Window Washing</h3>
-                <p>Residential</p>
-                <p>Commercial</p>
-                <p></p>
-            </div>
-            <div className="final-div">
-                <h3>Soft Cleaning</h3>
-                <p>Residential</p>
-                <p>Commercial</p>
-            </div>
+import "animate.css";
+
+export default function Services() {
+  return (
+    <div className="service-container1">
+      <h1 class="animate__animated animate__flipInY">Services</h1>
+      <div className="service-container">
+        <div className="first-div">
+          <h3 class="animate__animated animate__rotateInDownLeft">
+            Pressure Cleaning
+          </h3>
+          <div className="service-div">
+            <p class="animate__animated animate__fadeInLeftBig">Residential</p>
+            <p class="animate__animated animate__fadeInLeftBig">Commercial</p>
+            <p class="animate__animated animate__fadeInLeftBig">
+              Roof Cleaning
+            </p>
+          </div>
         </div>
-        <button className="button">Contact For A Free Quote</button>
+        <div className="second-div">
+          <h3 class="animate__animated animate__fadeInDown">Window Washing</h3>
+          <div className="service-div">
+            <p class="animate__animated animate__fadeInUpBig">Residential</p>
+            <p class="animate__animated animate__fadeInUpBig">Commercial</p>
+          </div>
         </div>
-    )
+        <div className="final-div">
+          <h3 class="animate__animated animate__rotateInDownRight">
+            Soft Cleaning
+          </h3>
+          <div className="service-div">
+            <p class="animate__animated animate__fadeInRightBig">Residential</p>
+            <p class="animate__animated animate__fadeInRightBig">Commercial</p>
+          </div>
+        </div>
+      </div>
+      <div className="button-div">
+        <Link to="/contact">
+          <button>Contact For A Free Quote</button>
+        </Link>
+      </div>
+      <div className="left-image-div">
+        <img className="left-pressure" src={PressureWasher}></img>
+      </div>
+      <div className="right-image-div">
+        <img className="right-pressure" src={PressureWasher2}></img>
+      </div>
+    </div>
+  );
 }
