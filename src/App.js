63c337730df4a5555e512cbdb175e644ec7acaf1
Evan St Martin <mantisTMD1@gmail.com>
2023-04-10 22:29:53 -0500

    got the images in. added favicon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav.js";
 import Footer from './components/Footer.js'
 
+
 function App() {
 
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,27 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import "../css/gallery.css";
+import 'animate.css';
+
+// images
+import driveway1before from "../assets/driveway1.jpg";
+import driveway1after from "../assets/driveway1after.jpg";
+import garagebefore from "../assets/garagebefore.jpg";
+import garageafter from "../assets/garageafter.jpg";
+import driveway2before from "../assets/driveway2before.jpg";
+import driveway2after from "../assets/driveway2after.jpg";
 
 export default function Gallery() {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 13,
+      items: 2,
       slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -23,37 +32,43 @@ export default function Gallery() {
 
   return (
     <>
-      <h1 class="animate__animated animate__flipInY">Gallery</h1>
-      <div className="gallery-container">
-        <Carousel
-          responsive={responsive}
-          infinite={true}
-          containerClass="carousel-container"
-        >
-          <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
+    <h1 className="animate__animated animate__flipInY" id = "gal-title">GALLERY</h1>
+    <div className="gallery-container">
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        swipeable={true}
+        draggable={true}
+        ssr={true} // means to render carousel on server-side.
+        autoPlay={true}
+        autoPlaySpeed={5000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+      >
+
+          <div className="car"> 
+            <img src = {driveway1before} alt="hi"></img>
+          </div>
+          <div className="car"> 
+          <img src={driveway1after} alt="hi"></img>
           </div>
           <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
+          <img src={garagebefore} alt="hi"></img>
           </div>
           <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
+          <img src={garageafter} alt="hi"></img>
           </div>
           <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
+          <img src={driveway2before} alt="hi"></img>
           </div>
           <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
+          <img src={driveway2after} alt="hi"></img>
           </div>
-          <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
-          </div>
-          <div className="car">
-            <img src="https://cdnassets.stihlusa.com/1630514494-rb400act2005.jpg?fit=crop&h=554&w=984" alt="hi"></img>
-          </div>
-        </Carousel>
-        ;
-      </div>
+     </Carousel>
+     </div>
+      ;
     </>
   );
 }
