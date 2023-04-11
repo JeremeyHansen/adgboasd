966dc0bda5d705933ae9c79719eb667526aa6f31
Jeremey Hansen <jeremeyhansen@yahoo.com>
2023-04-10 18:05:42 -0700

    added hamburger menu dropdown

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,21 @@ export default function About() {
         <h1 class="animate__animated animate__flipInY">About</h1>
         <div className="text-about">
           <p class="animate__animated animate__fadeIn">
-            <span>Pure <span className="quality">Quality</span></span> was founded in 2019 by Peter, a Charlotte native who
+            <span>Pure <span className="quality">Quality</span></span> was founded in 2019 by John Paul, a Charlotte native who
             has always had a passion for keeping his community clean and
-            beautiful. Starting out as a one-man operation, Peter used his
+            beautiful. Starting out as a one-man operation, John Paul used his
             pressure washing expertise to clean driveways, sidewalks, and homes
-            for his neighbors. Over the years, Peter's reputation for quality
+            for his neighbors. Over the years, John Paul's reputation for quality
             work and exceptional customer service grew, and he soon found
             himself with more work than he could handle on his own. In 2020,
-            Peter hired his first employee, and the business has been growing
+            John Paul hired his first employee, and the business has been growing
             steadily ever since. Today, <span>Pure <span className="quality">Quality</span></span> is one of the most trusted
             names in the Charlotte area when it comes to pressure washing
-            services. With a team of dozens of highly skilled and trained
+            services. With a team of highly skilled and trained
             employees, <span>Pure <span className="quality">Quality</span></span> is equipped to handle projects of any size,
             from small residential jobs to large commercial properties. Despite
             its growth, <span>Pure <span className="quality">Quality</span></span> has remained committed to the same values
-            that Peter founded the business on: honesty, integrity, and a
+            that John Paul founded the business on: honesty, integrity, and a
             commitment to excellence. Whether you need your driveway pressure
             washed or your entire building cleaned, you can trust <span>Pure <span className="quality">Quality</span></span>
             to get the job done right.
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,21 @@
 import "../css/navbar.css";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { useState } from "react";
+import { Spin as Hamburger } from "hamburger-react";
 
 export default function Nav() {
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(!open);
+  }
 
   return (
     <nav className="nav">
       <Link to="/" className="nav-title">
-        <span className="site-title">Pure <span className="quality">Quality</span></span>
+        <span className="site-title">
+          Pure <span className="quality">Quality</span>
+        </span>
       </Link>
       <ul className="desktop-nav">
         <CustomLink to="/about">About</CustomLink>
@@ -14,6 +23,17 @@ export default function Nav() {
         <CustomLink to="/gallery">Gallery</CustomLink>
         <CustomLink to="/contact">Contact</CustomLink>
       </ul>
+      <div onClick={handleOpen} className="hamburger">
+        <Hamburger className="hammy" />
+      </div>
+      {open && (
+        <ul className="mobile-nav" onClick={handleOpen}>
+          <CustomLink to="/about">About</CustomLink>
+          <CustomLink to="/services">Services</CustomLink>
+          <CustomLink to="/gallery">Gallery</CustomLink>
+          <CustomLink to="/contact">Contact</CustomLink>
+        </ul>
+      )}
     </nav>
   );
 }
